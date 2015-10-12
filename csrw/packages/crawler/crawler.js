6b12f7ec2c648479432b62d9f219e61758308585
Yonatan Wolowelsky <grmmph@gmail.com>
2015-10-12 09:50:03 +0200

    Add screenshot for username, support full urls

--- csrw/packages/crawler/crawler.js
+++ csrw/packages/crawler/crawler.js
@@ -1,9 +1,17 @@
 if (Meteor.isServer) {
 	Meteor.methods({
-		lookupUsername: function (username) {
+		lookupUsername: function (arg) {
 			var cheerio = Npm.require('cheerio');
 			var request = Npm.require('request');
-			var url = 'https://www.couchsurfing.com/people/' + username;
+			var username = '';
+			var url = '';
+			if (arg.lastIndexOf('www.couchsurfing') !== -1) {
+				url = arg;
+				username = arg.slice(arg.lastIndexOf('people/') + 7, arg.length)
+			} else {
+				username = arg;
+				url = 'https://www.couchsurfing.com/people/' + arg;
+			}
 			var async = Async.runSync(function(done) {
 				HTTP.call('GET', url, function (err, res) {
 					if (err) {
